821db00c6820bdf38c97c5b482dcd76d873d2e33
Berkay Polat <berkaypolat@Berkays-MacBook-Pro.local>
2025-01-27 15:05:32 -0500

    ML algorithm is integrated to the app using the Flask API endpoint.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,13 +5,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pickle
 import numpy as np
-from quote_upvote.models import Quote
+#from quote_upvote.models import Quote
 
 app = Flask(__name__)
 
 with open("./quote_upvote/quotes.pkl", "rb") as f:
     quotes = pickle.load(f)
-    print(quotes)
 
 with open("./quote_upvote/similarity_matrix.pkl", "rb") as f:
     similarity_matrix = pickle.load(f)
@@ -21,8 +20,8 @@ def recommend_similar_quote(liked_quote_index):
     similarity_row = similarity_matrix[liked_quote_index].toarray().flatten()
     similar_indices = np.argsort(similarity_row)[::-1] ## indexing: [start:stop:step]
 
-    similar_quote = [quotes[i].quote for i in similar_indices if i != liked_quote_index]
-    similar_quote_author = [quotes[i].author for i in similar_indices if i != liked_quote_index]
+    similar_quote = [quotes[i]['quote'] for i in similar_indices if i != liked_quote_index]
+    similar_quote_author = [quotes[i]['author'] for i in similar_indices if i != liked_quote_index]
     returned_object = {'quote': similar_quote[0], 'author': similar_quote_author[0]}
     return returned_object
 
