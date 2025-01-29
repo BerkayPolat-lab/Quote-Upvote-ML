89a09547e9049988019edc247cc0694fb2e613bc
Mert Bildirici <hmertbildirici@gmail.com>
2025-01-28 22:57:16 -0500

    first quote appears faster now

--- firebase/fetchQuotes.js
+++ firebase/fetchQuotes.js
@@ -1,19 +1,30 @@
 import { getFirestore, collection, getDocs } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Initialize Firestore
 const db = getFirestore();
 
 export const fetchQuotes = async () => {
-  try {
-    const quotesCollection = collection(db, "quotes");
-    const snapshot = await getDocs(quotesCollection);
-    const quotes = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    return quotes;
-  } catch (error) {
-    console.error("Error fetching quotes:", error);
-    throw error;
-  }
+    try {
+        // First, try to get quotes from AsyncStorage
+        const cachedQuotes = await AsyncStorage.getItem("quotes");
+        if (cachedQuotes) {
+            return JSON.parse(cachedQuotes);
+        }
+
+        // If no cached quotes, fetch from Firestore
+        const quotesCollection = collection(db, "quotes");
+        const snapshot = await getDocs(quotesCollection);
+        const quotes = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+
+        // Store fetched quotes in AsyncStorage for future use
+        await AsyncStorage.setItem("quotes", JSON.stringify(quotes));
+
+        return quotes;
+    } catch (error) {
+        console.error("Error fetching quotes:", error);
+        throw error;
+    }
 };
