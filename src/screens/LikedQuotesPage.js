3e30fb117f37a2ecce98ada8f8f94c275f45b880
Berkay Polat <berkaypolat@Berkays-MacBook-Pro.local>
2025-01-31 00:25:57 -0500

    Firestore collection is updated when a quote is liked.

--- src/screens/LikedQuotesPage.js
+++ src/screens/LikedQuotesPage.js
@@ -1,11 +1,47 @@
-import {ScrollView, Text} from 'react-native';
+import { useEffect } from 'react';
+import {ScrollView, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
-const LikedQuotesPage = () => {
+
+const LikedQuotesPage = ({navigation}) => {
+
+    const handleHomeNavigation = () => {
+        try {
+            navigation.replace('Home'); // Navigate to the Home screen
+        } catch (error) {
+            setError(error.message);    
+        }
+    };
+
     return (
-        <ScrollView>
-            <Text>Liked Quotes</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <TouchableOpacity style={styles.HomeButton} onPress={handleHomeNavigation}>
+                <Text style={styles.HomeButtonText}>Home</Text>
+            </TouchableOpacity>
+            <Text style={styles.title}> Liked Quotes</Text>
+
         </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 20,
+    },
+    HomeButton: {
+        position: 'absolute', // Position in the top-left corner
+        top: 10,
+        left: 10,
+        backgroundColor: 'transparent', // Transparent background
+        padding: 5,
+    },
+    HomeButtonText: {
+        color: '#007AFF', // Blue color (similar to iOS default)
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center', marginTop: 20},
+});
 
 export default LikedQuotesPage;
